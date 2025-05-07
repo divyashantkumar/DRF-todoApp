1db84d9c92194c8b261a11fa618b0224b7c48fcb
divyashantkumar <divyashantkumar000@gmail.com>
2025-05-07 11:14:24 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo';
+  task = "";
+
+  taskList: Array<string> = [];
+  isUpdate = false;
+  updateIndex: number | undefined;
+
+  addTask() {
+    // add Task
+    this.taskList.push(this.task);
+    console.log(this.taskList);
+
+    // Clear the Task Input Field
+    this.task = "";
+
+  }
+
+
+  deleteTask(idx: number) {
+    this.taskList.splice(idx, 1);
+  }
+
+  editFunc(index: number) {
+    this.updateIndex = index;
+    this.isUpdate = true;
+    this.task = this.taskList[index]
+  }
+
+  updateTask() {
+    const updateTaskIndex:number = this.updateIndex as number;
+    const updatedTask = this.task;
+
+    this.taskList[updateTaskIndex] = updatedTask;
+
+    // Clear the Task Input Field
+    this.task = "";
+
+    // Toggle add and update Button
+    this.isUpdate = false;
+  }
 }
